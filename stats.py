0ce4ede58a4cf1147a2c42285756c3ee116dc044
Chrisc-80 <chrisc@wp.pl>
2025-05-22 08:35:54 +0200

    H: Add get chars dictionanry in stats.py and modify main.py

--- stats.py
+++ stats.py
@@ -1,10 +1,16 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def get_num_words():
-    book_text = get_book_text("books/frankenstein.txt")
-    list_of_words = book_text.split()
-    number = len(list_of_words)
-    print(f"{number} words found in the document")
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
